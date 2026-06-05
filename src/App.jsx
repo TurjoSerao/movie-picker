7299615d1f2e9a6e670ae8f22cedc6384e56db83
devserao <seraotourjo@gmail.com>
2026-06-06 02:48:00 +0600

    added function to the favorites page and favourite button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import { MovieProvider } from "./context/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
         <Routes>
@@ -13,7 +14,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
